62ba15be990132597df30ac272036652ca884b5d
andres-martin <andresmartin266@gmail.com>
2020-01-26 17:17:57 -0500

    [add] add nested comments

--- src/server.js
+++ src/server.js
@@ -1,4 +1,4 @@
-import { buildSchema, graphql } from 'graphql';
+// import { buildSchema, graphql } from 'graphql';
 import express from 'express';
 import graphqlHTTP from 'express-graphql';
 import { makeExecutableSchema } from 'graphql-tools';
@@ -10,12 +10,21 @@ const typeDefs = `
     url: String!
     description: String
     author: User!
+    comments: [Comment]
   }
   
   type User {
     id: Int!
     username: String!
     about: String
+  }
+
+  type Comment {
+    id: Int!
+    parent: Comment
+    comments: [Comment]
+    author: User!
+    content: String!
   }
 
   type Query {
@@ -34,7 +43,22 @@ const links = [
 const users = [
   { id: 0, username: 'user1', about: 'The first user' },
   { id: 1, username: 'user2', about: 'The second user' },
+  { id: 2, username: 'user3', about: 'The third user' },
 ];
+
+const commentsList = [
+  { id: 0, parent: null, author: 0, content: 'Comment 1' },
+  { id: 1, parent: 0, author: 1, content: 'Comment 2' },
+  { id: 2, parent: 1, author: 0, content: 'Comment 3' },
+  { id: 3, parent: 0, author: 2, content: 'Comment 4' },
+  { id: 4, parent: null, author: 2, content: 'Comment 5' },
+];
+
+function getComments(commentID) {
+  const comments = commentsList.filter(comment => comment.parent === commentID);
+  if (comments.length > 0) return comments;
+  return null;
+}
 
 const resolvers = {
   Query: {
@@ -46,6 +70,11 @@ const resolvers = {
   Link: {
     // this resolver is for the nested query with User model
     author: ({ author }) => find(users, { id: author }),
+    comments: ({ comments }) => comments.map(i => find(commentsList, { id: i })),
+  },
+  Comment: {
+    author: ({ author }) => find(users, { id: author }),
+    comments: ({ id }) => getComments(id),
   },
 };
 
